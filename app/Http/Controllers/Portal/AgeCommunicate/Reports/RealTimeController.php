ee11267de410631e226c10f19430b64ee2848e4e
Carlos Neto <carlos.neto@agetelecom.com.br>
2024-05-06 13:29:02 -0300

    regua de cobranca

--- app/Http/Controllers/Portal/AgeCommunicate/Reports/RealTimeController.php
+++ app/Http/Controllers/Portal/AgeCommunicate/Reports/RealTimeController.php
@@ -28,30 +28,34 @@ class RealTimeController extends Controller
         // Executa a consulta
         $reports = $query->get();
 
+
         // Inicializa uma flag para verificar se houve mudanças
         $changes = false;
 
         foreach ($reports as $report) {
             // Verifica se há diferença entre os dados atuais e os novos dados
-            if ($report->status !== $result[0]['status']['groupId'] ||
+            if ($report->status !== $result[0]['status']['id'] ||
                 $report->status_descricao !== $result[0]['status']['id']) {
                 $changes = true;
+
 
                 // Atualiza o registro já que os dados são diferentes
                 $report->update([
                     'status' => $result[0]['status']['id'],
                     'status_descricao' => $result[0]['status']['id']
                 ]);
-            } else {
-                $log = ReportSmsLog::create([
-                    'envio_id' => $report->id,
-                    'bulk_id' => isset($result[0]['bulkId']) ? $result[0]['bulkId'] : 'envio_individual',
-                    'mensagem_id' => $result[0]['messageId'],
-                    'enviado_para' => $result[0]['to'],
-                    'resposta_webhook' => json_encode($result),
-                    'status' => $changes ? 2 : 3
-                ]);
+
+
             }
+
+            $log = ReportSmsLog::create([
+                'envio_id' => $report->id,
+                'bulk_id' => isset($result[0]['bulkId']) ? $result[0]['bulkId'] : 'envio_individual',
+                'mensagem_id' => $result[0]['messageId'],
+                'enviado_para' => $result[0]['to'],
+                'resposta_webhook' => json_encode($result),
+                'status' => $changes ? 2 : 3
+            ]);
         }
 
 
