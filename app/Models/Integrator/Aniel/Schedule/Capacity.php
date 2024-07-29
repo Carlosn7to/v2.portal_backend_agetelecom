f274d160516baa1a1d73afcba14ea8113405ca92
Carlos Neto <carlos.neto@agetelecom.com.br>
2024-07-29 17:30:07 -0300

    correcao status os

--- app/Models/Integrator/Aniel/Schedule/Capacity.php
+++ app/Models/Integrator/Aniel/Schedule/Capacity.php
@@ -20,6 +20,7 @@ class Capacity extends Model
         'capacidade',
         'utilizado',
         'status',
+        'hora_fechamento',
         'atualizado_por',
         'motivo_fechamento'
     ];
