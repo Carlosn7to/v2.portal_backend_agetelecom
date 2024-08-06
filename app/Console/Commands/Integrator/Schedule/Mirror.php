9093cbe655f130644bb3815f760678d852229278
Carlos Neto <carlos.neto@agetelecom.com.br>
2024-08-06 10:51:01 -0300

    correcao mirror communicate

--- app/Console/Commands/Integrator/Schedule/Mirror.php
+++ app/Console/Commands/Integrator/Schedule/Mirror.php
@@ -34,37 +34,37 @@ class Mirror extends Command
         $info = new InfoOrder();
 
         $info->__invoke();
-
-        $startDate = Carbon::now()->subDays(10)->startOfDay();
-        $uniqueDates = \App\Models\Integrator\Aniel\Schedule\Mirror::where('data_agendamento', '>=', $startDate)
-            ->get(['data_agendamento'])
-            ->map(function ($item) {
-                return Carbon::parse($item->data_agendamento)->toDateString();
-            })
-            ->unique()
-            ->values();
-
-        $jobs = [];
-
-        foreach ($uniqueDates as $date) {
-
-            $ordersVoalle = ImportOrder::whereDate('data_agendamento', $date)
-                ->get(['protocolo', 'tipo_servico', 'data_agendamento', 'node as localidade', 'status_id', 'cliente_id'])->toArray();
-            // Adicionar a job ao array de jobs
-            $jobs[] = new UpdateMirrorAniel($ordersVoalle);
-        }
-
-        // Despacha os jobs em um batch
-        \Bus::batch($jobs)->then(function (Batch $batch) {
-            // Todos os jobs foram processados com sucesso
-            \Log::info('Todos os jobs foram processados com sucesso.');
-        })->catch(function (Batch $batch, \Throwable $e) {
-            // Algum job falhou
-            \Log::info('Algum job falhou.');
-        })->finally(function (Batch $batch) {
-            // Todos os jobs foram concluídos
-            \Log::info('Todos os jobs foram concluídos.');
-        })->dispatch();
+//
+//        $startDate = Carbon::now()->subDays(10)->startOfDay();
+//        $uniqueDates = \App\Models\Integrator\Aniel\Schedule\Mirror::where('data_agendamento', '>=', $startDate)
+//            ->get(['data_agendamento'])
+//            ->map(function ($item) {
+//                return Carbon::parse($item->data_agendamento)->toDateString();
+//            })
+//            ->unique()
+//            ->values();
+//
+//        $jobs = [];
+//
+//        foreach ($uniqueDates as $date) {
+//
+//            $ordersVoalle = ImportOrder::whereDate('data_agendamento', $date)
+//                ->get(['protocolo', 'tipo_servico', 'data_agendamento', 'node as localidade', 'status_id', 'cliente_id'])->toArray();
+//            // Adicionar a job ao array de jobs
+//            $jobs[] = new UpdateMirrorAniel($ordersVoalle);
+//        }
+//
+//        // Despacha os jobs em um batch
+//        \Bus::batch($jobs)->then(function (Batch $batch) {
+//            // Todos os jobs foram processados com sucesso
+//            \Log::info('Todos os jobs foram processados com sucesso.');
+//        })->catch(function (Batch $batch, \Throwable $e) {
+//            // Algum job falhou
+//            \Log::info('Algum job falhou.');
+//        })->finally(function (Batch $batch) {
+//            // Todos os jobs foram concluídos
+//            \Log::info('Todos os jobs foram concluídos.');
+//        })->dispatch();
 
         return 0;
     }
