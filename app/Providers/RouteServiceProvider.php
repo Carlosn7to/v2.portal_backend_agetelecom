7f6313862524b932d34d0c4af5ba04dbd10eabbe
Carlos Neto <carlos.neto@agetelecom.com.br>
2024-09-24 15:57:08 -0300

    test cors portal

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -77,6 +77,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('portal/agereport/')
                 ->group(base_path('routes/api/portal/ageReport/api.php'));
 
+            // Rotas para os serviços diversos
+            Route::middleware('portal')
+                ->prefix('portal/services/')
+                ->group(base_path('routes/api/portal/services/api.php'));
+
             // Rotas para a integração de boletos Digitro x Voalle
             Route::middleware('portal')
                 ->prefix('integrator/voalle/')
